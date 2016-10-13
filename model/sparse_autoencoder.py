e258d9413c3f55a02a50a2b7e815820bb7a3f160
ryoito <r0735nj5058@gmail.com>
2016-10-13 12:15:14 +0900

    utilized methods defined in parent class

--- model/sparse_autoencoder.py
+++ model/sparse_autoencoder.py
@@ -3,26 +3,15 @@ from autoencoder import Autoencoder
 
 
 class SparseAutoencoder(Autoencoder):
-    """autoencoder to learn overcomplete features"""
-    def __init__(self, inputs, n_input, n_hidden, sparsity):
-        self.inputs = inputs
-        self.n_input = n_input
-        self.n_hidden = n_hidden
-        self.sparsity = sparsity
+    """autoencoder to learn sparse features"""
+    def __init__(self, inputs, n_input, n_hidden):
+        super(SparseAutoencoder, self).__init__(inputs, n_input, n_hidden)
 
-        self._forward()
-
-    def reconst_error(self):
-        return tf.nn.l2_loss(self.inputs - self.outputs) / tf.to_float(tf.shape(self.inputs)[0])
-
-    def kl_divergence(self):
+    def kl_divergence(self, sparsity):
         active_rates = tf.reduce_mean(self.h, 0)
         return tf.reduce_sum(
-            self.sparsity * tf.log(self.sparsity / active_rates)
-            + (1 - self.sparsity) * tf.log((1 - self.sparsity) / (1 - active_rates)))
+            sparsity * tf.log(sparsity / active_rates)
+            + (1 - sparsity) * tf.log((1 - sparsity) / (1 - active_rates)))
 
     def weight_decay(self):
         return tf.nn.l2_loss(self.fc1w) + tf.nn.l2_loss(self.fc2w)
-
-    def cost(self):
-        return self._reconst_error() + self._kl_divergence()
