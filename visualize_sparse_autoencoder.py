05466c6a66421b6fbbce024a580e84bb421cd2f4
ryoito <r0735nj5058@gmail.com>
2016-10-13 18:01:09 +0900

    added default name for output file

--- visualize_sparse_autoencoder.py
+++ visualize_sparse_autoencoder.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('--model', type=str, default="sparse_autoencoder.ckpt", help='filename of the model to visualize')
     parser.add_argument('--n_input', type=int, default=28 * 28, help='number of input nodes')
     parser.add_argument('--n_hidden', type=int, default=400, help='number of hidden nodes')
-    parser.add_argument('--output', type=str, default="", help="filename to save learned filters")
+    parser.add_argument('--output', type=str, default="filters.png", help="filename to save learned filters")
     args = parser.parse_args()
 
     inputs = tf.placeholder(tf.float32, [None, args.n_input])
